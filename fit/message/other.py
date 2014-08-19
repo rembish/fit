f59092592170a0b54d4ce32120f7f1b5ce2494f1
Aleksey Rembish <aleksey.rembish@firma.seznam.cz>
2014-08-19 17:07:27 +0200

    Meme Glob Message

--- fit/message/other.py
+++ fit/message/other.py
@@ -1,5 +1,12 @@
 from fit.message import Message
+from fit.types.extended import MessageIndex
+from fit.types.general import UInt32, Byte, UInt16
 
 
 class MemoGlob(Message):
     msg_type = 145
+
+    part_index = UInt32(250)
+    memo = Byte(0)
+    message_number = UInt16(1)
+    message_index = MessageIndex(2)
